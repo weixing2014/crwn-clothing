5707607ab76ad72c8100374e0bf360139e80de1f
xing_wei <xing.wei@airbnb.com>
2021-03-04 23:27:55 -0800

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage.component';
+
+export function HatsPage(props) {
+  return <div>HATS PAGE</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
